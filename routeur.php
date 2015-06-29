2c843492f294b97b597f2a181dc6d6c89f1cbdad
Soka <nicolas.lorant@loretic.com>
2015-06-29 17:37:18 +0200

    Création du file routeur.php

---
+++ routeur.php
@@ -0,0 +1,12 @@
+<?php $version_git ="master";
+?>
+<html>
+	<head>
+		<title>Routeur <?=version_git?></title>
+	</head>
+	<body>
+		<?php 
+			echo "Routeur Test".$version_git;
+		?>
+	</body>
+</html>
