9c2a1e1a8a0a190e910b0179cefff8a3b3214771
Soka <nicolas.lorant@loretic.com>
2015-06-29 17:33:37 +0200

    Création du file index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php $version_git ="master";
+?>
+<html>
+	<head>
+		<title>Index <?=version_git?></title>
+	</head>
+	<body>
+		<?php 
+			echo "Test".$version_git;
+		?>
+	</body>
+</html>
+	
+	
+	
+	
